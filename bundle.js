eca4a1d504b9308a34328d7751149f04ed3c5ab8
Yvo niedrich <yvo.niedrich@gmail.com>
2016-05-21 22:18:58 +0200

    Plugin in ES2015

--- bundle.js
+++ bundle.js
@@ -1,43 +1,45 @@
+require('./lib/polyfills.js');
+
 module.exports = {
     'models': {
-        'base' : require("./src/model/base.js"),
-        'dummy' : require("./src/model/dummy.js")
+        'base' : require("./lib/model/base.js"),
+        'dummy' : require("./lib/model/dummy.js")
     },
 
     'components': {
-        'dependency': require("./src/model/components/dependency.js"),
-        'version':    require("./src/model/components/version.js"),
-        'node':       require("./src/model/components/treenode.js")
+        'dependency': require("./lib/model/components/dependency.js"),
+        'version':    require("./lib/model/components/version.js"),
+        'node':       require("./lib/model/components/treenode.js")
     },
 
     'illustrators': {
-        'base':      require("./src/illustrator/base.js"),
-        'evostreet': require("./src/illustrator/evostreet.js"),
-        'districts': require("./src/illustrator/district.js")
+        'base':      require("./lib/illustrator/base.js"),
+        'evostreet': require("./lib/illustrator/evostreet.js"),
+        'districts': require("./lib/illustrator/district.js")
     },
 
     'containers': {
-        'base':     require("./src/illustrator/container/base.js"),
-        'row':      require("./src/illustrator/container/row.js"),
-        'lightmap': require("./src/illustrator/container/lightmap.js"),
-        'grid':     require("./src/illustrator/container/grid.js")
+        'base':     require("./lib/illustrator/container/base.js"),
+        'row':      require("./lib/illustrator/container/row.js"),
+        'lightmap': require("./lib/illustrator/container/lightmap.js"),
+        'grid':     require("./lib/illustrator/container/grid.js")
     },
 
     // TODO: Brauche ich wirklich Shapes???
     'shapes': {
-        'base':     require("./src/illustrator/shapes/base.js"),
-        'house':    require("./src/illustrator/shapes/house.js"),
-        'street':   require("./src/illustrator/shapes/street.js"),
-        'platform': require("./src/illustrator/shapes/platform.js")
+        'base':     require("./lib/illustrator/shapes/base.js"),
+        'house':    require("./lib/illustrator/shapes/house.js"),
+        'street':   require("./lib/illustrator/shapes/street.js"),
+        'platform': require("./lib/illustrator/shapes/platform.js")
     },
 
     // TODO!
     'rules': {
-        'editor-to-width':             require("./src/illustrator/rules/editor-to-width.js"),
-        'lighten-platform-with-level': require("./src/illustrator/rules/lighten-platform-with-level.js"),
-        'loc-to-height':               require("./src/illustrator/rules/loc-to-height.js"),
-        'opacity-if-not-in-version':   require("./src/illustrator/rules/opacity-if-not-in-version.js"),
-        'package-to-color':            require("./src/illustrator/rules/package-to-color.js"),
-        'save-first-version':          require("./src/illustrator/rules/save-first-version.js")
+        'editor-to-width':             require("./lib/illustrator/rules/editor-to-width.js"),
+        'lighten-platform-with-level': require("./lib/illustrator/rules/lighten-platform-with-level.js"),
+        'loc-to-height':               require("./lib/illustrator/rules/loc-to-height.js"),
+        'opacity-if-not-in-version':   require("./lib/illustrator/rules/opacity-if-not-in-version.js"),
+        'package-to-color':            require("./lib/illustrator/rules/package-to-color.js"),
+        'save-first-version':          require("./lib/illustrator/rules/save-first-version.js")
     }
 };
