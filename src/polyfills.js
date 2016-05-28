adbfbdf14f7c84321025ed6d3eed63bb2ccead63
Yvo niedrich <yvo.niedrich@gmail.com>
2016-05-28 11:46:22 +0200

    Safe polyfill

--- src/polyfills.js
+++ src/polyfills.js
@@ -4,9 +4,20 @@
 // * Chrome 50
 // * Firefox 45
 
-require('core-js/fn/set');
-require('core-js/fn/array/index-of.js');
-require('core-js/fn/object/get-own-property-descriptor.js');
-require('core-js/fn/object/get-own-property-descriptors.js');
-require('core-js/fn/object/get-own-property-names.js');
-require('core-js/fn/object/get-own-property-symbols.js');
+var requiredPolyfills = [
+    'core-js/fn/set',
+    'core-js/fn/array/index-of.js',
+    'core-js/fn/object/get-own-property-descriptor.js',
+    'core-js/fn/object/get-own-property-descriptors.js',
+    'core-js/fn/object/get-own-property-names.js',
+    'core-js/fn/object/get-own-property-symbols.js'
+];
+
+for (var poly of requiredPolyfills) {
+    try {
+        require(poly);
+    } catch (err) {
+        console.log('Could not load polyfill: ' + poly);
+        console.log(err);
+    }
+}
