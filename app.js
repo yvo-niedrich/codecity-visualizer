d1d95eb525a7656c199ac004eedf5448f8d9802e
Ungolianth <yvo.niedrich@gmail.com>
2016-07-08 14:53:38 +0200

    Dateiendung für Module entfernt

--- app.js
+++ app.js
@@ -1,53 +1,54 @@
-require('./lib/polyfills.js');
+require('./lib/polyfills');
 
 module.exports = {
     'models': {
-        'base' : require("./lib/model/base.js"),
-        'dummy' : require("./lib/model/dummy.js")
+        'base' : require("./lib/model/base"),
+        'dummy' : require("./lib/model/dummy")
+        // 'dummy' : require("./lib/model/DummyModel")
     },
 
     'components': {
-        'dependency': require("./lib/model/components/Dependency.js"),
-        'version':    require("./lib/model/components/Version.js"),
-        'node':       require("./lib/model/components/TreeNode.js")
+        'dependency': require("./lib/model/components/Dependency"),
+        'version':    require("./lib/model/components/Version"),
+        'node':       require("./lib/model/components/TreeNode")
     },
 
     'illustrators': {
-        'base':      require("./lib/illustrator/base.js"),
-        'evostreet': require("./lib/illustrator/evostreet.js"),
-        'district': require("./lib/illustrator/district.js")
+        'base':      require("./lib/illustrator/base"),
+        'evostreet': require("./lib/illustrator/evostreet"),
+        'district': require("./lib/illustrator/district")
     },
 
     'containers': {
-        'base':     require("./lib/illustrator/container/base-universal.js"),
-        'row':      require("./lib/illustrator/container/universal/row.js"),
-        'lightmap': require("./lib/illustrator/container/universal/lightmap.js"),
-        'grid':     require("./lib/illustrator/container/universal/grid.js")
+        'base':     require("./lib/illustrator/container/base-universal"),
+        'row':      require("./lib/illustrator/container/universal/row"),
+        'lightmap': require("./lib/illustrator/container/universal/lightmap"),
+        'grid':     require("./lib/illustrator/container/universal/grid")
     },
 
     'shapes': {
-        'base':     require("./lib/illustrator/shapes/base.js"),
-        'house':    require("./lib/illustrator/shapes/house.js"),
-        'street':   require("./lib/illustrator/shapes/street.js"),
-        'platform': require("./lib/illustrator/shapes/platform.js")
+        'base':     require("./lib/illustrator/shapes/base"),
+        'house':    require("./lib/illustrator/shapes/house"),
+        'street':   require("./lib/illustrator/shapes/street"),
+        'platform': require("./lib/illustrator/shapes/platform")
     },
 
     'rules': {
-        'base' : require('./lib/illustrator/rules/base.js'),
-        'universal': require('./lib/illustrator/rules/universal.js'),
+        'base' : require('./lib/illustrator/rules/base'),
+        'universal': require('./lib/illustrator/rules/universal'),
 
         'math': {
-            'linear'     : require('./lib/illustrator/rules/math/linear.js'),
-            'logarithmic': require('./lib/illustrator/rules/math/logarithmic.js'),
-            'exponential': require('./lib/illustrator/rules/math/exponential.js')
+            'linear'     : require('./lib/illustrator/rules/math/linear'),
+            'logarithmic': require('./lib/illustrator/rules/math/logarithmic'),
+            'exponential': require('./lib/illustrator/rules/math/exponential')
         },
         'color': {
-            'gradient'   : require('./lib/illustrator/rules/color/gradient.js'),
-            'assigned'   : require('./lib/illustrator/rules/color/assigned.js')
+            'gradient'   : require('./lib/illustrator/rules/color/gradient'),
+            'assigned'   : require('./lib/illustrator/rules/color/assigned')
         }
     },
 
     'helper': {
-        'attributes': require('./lib/model/helper/attributeExtractor.js')
+        'attributes': require('./lib/model/helper/attributeExtractor')
     }
 };
